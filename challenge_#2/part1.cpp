8613ba12e5f3916b1ba03a1cac23c8ec2c3ccd1a
Marcos <marcos510hernandez@gmail.com>
2021-12-21 14:18:28 -0800

    part1 solution

---
+++ challenge_#2/part1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+int main(){
+    std::string movement, sMovement, sMove;
+    int horizontal = 0, depth = 0, nMove;
+    std::ifstream inFile;
+    inFile.open("input.txt");
+    if(!inFile){
+        std::cout << "error: unable to open input file";
+        exit(1);            
+    }
+      
+    while(inFile >> sMovement >> sMove){                      //*
+        nMove = std::stoi(sMove);                  //*     while loop to asign input file to array
+        if(sMovement == "forward")
+            horizontal += nMove;
+        else if(sMovement == "down")
+            depth += nMove;
+        else if(sMovement == "up")
+            depth -= nMove;
+    }
+    std::cout << "depth: " << depth << std::endl;
+    std::cout << "horizontal: " << horizontal << std::endl;
+    std::cout << horizontal * depth;
+}
